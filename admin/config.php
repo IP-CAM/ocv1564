62f70db9428b4c08145fb48dab81c354601540d2
telnetuser <telnetuser@yeah.net>
2017-11-27 19:40:22 +0800

    test

--- admin/config.php
+++ admin/config.php
@@ -1,26 +1,26 @@
 <?php
-$CONFIG_ADMIN_DOMAIN = $_SERVER['HTTP_HOST'];
-$CONFIG_ADMIN_PATH = $_SERVER['DOCUMENT_ROOT'];
+define('CONFIG_ADMIN_DOMAIN','www.ocv1564.net');
+define('CONFIG_ADMIN_PATH',dirname(dirname(__FILE__)));
 // HTTP
-define('HTTP_SERVER', 'http://'.$CONFIG_ADMIN_DOMAIN.'/admin/');
-define('HTTP_CATALOG', 'http://'.$CONFIG_ADMIN_DOMAIN.'/');
+define('HTTP_SERVER', 'http://'.CONFIG_ADMIN_DOMAIN.'/admin/');
+define('HTTP_CATALOG', 'http://'.CONFIG_ADMIN_DOMAIN.'/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://'.$CONFIG_ADMIN_DOMAIN.'/admin/');
-define('HTTPS_CATALOG', 'http://'.$CONFIG_ADMIN_DOMAIN.'/');
+define('HTTPS_SERVER', 'http://'.CONFIG_ADMIN_DOMAIN.'/admin/');
+define('HTTPS_CATALOG', 'http://'.CONFIG_ADMIN_DOMAIN.'/');
 
 // DIR
-define('DIR_APPLICATION', $CONFIG_ADMIN_PATH.'/admin/');
-define('DIR_SYSTEM', $CONFIG_ADMIN_PATH.'/system/');
-define('DIR_DATABASE', $CONFIG_ADMIN_PATH.'/system/database/');
-define('DIR_LANGUAGE', $CONFIG_ADMIN_PATH.'/admin/language/');
-define('DIR_TEMPLATE', $CONFIG_ADMIN_PATH.'/admin/view/template/');
-define('DIR_CONFIG', $CONFIG_ADMIN_PATH.'/system/config/');
-define('DIR_IMAGE', $CONFIG_ADMIN_PATH.'/image/');
-define('DIR_CACHE', $CONFIG_ADMIN_PATH.'/system/cache/');
-define('DIR_DOWNLOAD', $CONFIG_ADMIN_PATH.'/download/');
-define('DIR_LOGS', $CONFIG_ADMIN_PATH.'/system/logs/');
-define('DIR_CATALOG', $CONFIG_ADMIN_PATH.'/catalog/');
+define('DIR_APPLICATION', CONFIG_ADMIN_PATH.'/admin/');
+define('DIR_SYSTEM', CONFIG_ADMIN_PATH.'/system/');
+define('DIR_DATABASE', CONFIG_ADMIN_PATH.'/system/database/');
+define('DIR_LANGUAGE', CONFIG_ADMIN_PATH.'/admin/language/');
+define('DIR_TEMPLATE', CONFIG_ADMIN_PATH.'/admin/view/template/');
+define('DIR_CONFIG', CONFIG_ADMIN_PATH.'/system/config/');
+define('DIR_IMAGE', CONFIG_ADMIN_PATH.'/image/');
+define('DIR_CACHE', CONFIG_ADMIN_PATH.'/system/cache/');
+define('DIR_DOWNLOAD', CONFIG_ADMIN_PATH.'/download/');
+define('DIR_LOGS', CONFIG_ADMIN_PATH.'/system/logs/');
+define('DIR_CATALOG', CONFIG_ADMIN_PATH.'/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
--- config.php
+++ config.php
@@ -1,23 +1,23 @@
 <?php
-$CONFIG_STORE_DOMAIN = $_SERVER['HTTP_HOST'];
-$CONFIG_STORE_PATH = $_SERVER['DOCUMENT_ROOT'];
+define('CONFIG_STORE_DOMAIN','www.ocv1564.net');
+define('CONFIG_STORE_PATH',dirname(__FILE__));
 // HTTP
-define('HTTP_SERVER', 'http://'.$CONFIG_STORE_DOMAIN.'/');
+define('HTTP_SERVER', 'http://'.CONFIG_STORE_DOMAIN.'/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://'.$CONFIG_STORE_DOMAIN.'/');
+define('HTTPS_SERVER', 'http://'.CONFIG_STORE_DOMAIN.'/');
 
 // DIR
-define('DIR_APPLICATION', $CONFIG_STORE_PATH.'/catalog/');
-define('DIR_SYSTEM', $CONFIG_STORE_PATH.'/system/');
-define('DIR_DATABASE', $CONFIG_STORE_PATH.'/system/database/');
-define('DIR_LANGUAGE', $CONFIG_STORE_PATH.'/catalog/language/');
-define('DIR_TEMPLATE', $CONFIG_STORE_PATH.'/catalog/view/theme/');
-define('DIR_CONFIG', $CONFIG_STORE_PATH.'/system/config/');
-define('DIR_IMAGE', $CONFIG_STORE_PATH.'/image/');
-define('DIR_CACHE', $CONFIG_STORE_PATH.'/system/cache/');
-define('DIR_DOWNLOAD', $CONFIG_STORE_PATH.'/download/');
-define('DIR_LOGS', $CONFIG_STORE_PATH.'/system/logs/');
+define('DIR_APPLICATION', CONFIG_STORE_PATH.'/catalog/');
+define('DIR_SYSTEM', CONFIG_STORE_PATH.'/system/');
+define('DIR_DATABASE', CONFIG_STORE_PATH.'/system/database/');
+define('DIR_LANGUAGE', CONFIG_STORE_PATH.'/catalog/language/');
+define('DIR_TEMPLATE', CONFIG_STORE_PATH.'/catalog/view/theme/');
+define('DIR_CONFIG', CONFIG_STORE_PATH.'/system/config/');
+define('DIR_IMAGE', CONFIG_STORE_PATH.'/image/');
+define('DIR_CACHE', CONFIG_STORE_PATH.'/system/cache/');
+define('DIR_DOWNLOAD', CONFIG_STORE_PATH.'/download/');
+define('DIR_LOGS', CONFIG_STORE_PATH.'/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
